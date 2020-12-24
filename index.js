5b7a65efd99ff301e6db00a75fec7d3400f6daff
Yash-Wasalwar-07 <71366827+Yash-Wasalwar-07@users.noreply.github.com>
2020-12-24 11:24:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 
-document.querySelector(".img1").setAttribute("src",`images/dice${randomNumber1}.png`);
-document.querySelector(".img2").setAttribute("src",`images/dice${randomNumber2}.png`);
+document.querySelector(".img1").setAttribute("src",`Dicee-Game/images/dice${randomNumber1}.png`);
+document.querySelector(".img2").setAttribute("src",`Dicee-Game/images/dice${randomNumber2}.png`);
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "🚩 Player1 wins!"
